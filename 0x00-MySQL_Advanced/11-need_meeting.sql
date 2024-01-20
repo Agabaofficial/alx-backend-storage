5ed47f5f8a16afa8b993f7160ec358bdbc53c088
Agabaofficial <agabaolivier85@gmail.com>
2024-01-20 13:00:04 -0600

    update

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,4 @@
--- Creates a view need_meeting that lists all students that have a score
+-- Creates a view need_meeting that lists all students that got a score which is 
 -- under 80 (strict) and no last_meeting or more than 1 month.
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
