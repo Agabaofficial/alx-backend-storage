8ff4c33d637bc0a3f8184850b3b590d215aa34b9
Agaba Olivier <125471253+Agabaofficial@users.noreply.github.com>
2024-01-20 12:47:34 -0600

    Update 101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,5 +1,5 @@
--- Creates a stored procedure ComputeAverageWeightedScoreForUsers that
--- computes and store the average weighted score for all students.
+-- Creates a stored procedure ComputeAverageWeightedScoreForUsers which will
+-- compute and store the average weighted score for all students.
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
